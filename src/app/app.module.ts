f7e33e366adeffa1dafdb97bc820c383bc284ca6
sidra.sm18@gmail.com <sidra.sm18@gmail.com>
2019-03-26 11:40:57 +0530

    Setting Up reactive form: 1. Creating a property to hold the form. 2. Importing ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { ReactiveFormsModule } from '@angular/forms'
 import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    // FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
