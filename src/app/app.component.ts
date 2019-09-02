5ddcd90117bb92c0c54edb0c0c2a6bb59783d545
sidramowlana <46183886+sidramowlana@users.noreply.github.com>
2019-09-02 18:25:15 +0530

    Creating a form object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray, FormArrayName } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  genders = ['male', 'female', 'other'];
-
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
   signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      "hobbies": new FormArray(null, Validators.required),
+      'gender': new FormControl('female')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+ 
 }
