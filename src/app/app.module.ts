285a81a8fa48e3af060c5168cd1261052b2ec5a6
sidramowlana <46183886+sidramowlana@users.noreply.github.com>
2019-09-02 18:27:21 +0530

    Import ReactiveFormsModule to create a reactive Form
    Remove the FormsModule it is for template driven

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'
-import { HttpModule } from '@angular/http'
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,9 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    // FormsModule,
-    ReactiveFormsModule,
-    HttpModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
